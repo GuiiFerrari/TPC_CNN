19e9f0516509a67f42998ece107842c1177479a9
GuiiFerrari <56280982+GuiiFerrari@users.noreply.github.com>
2022-02-28 15:17:59 -0300

    Base models

--- tpc_signal/baseline.py
+++ tpc_signal/baseline.py
@@ -6,30 +6,26 @@ from tensorflow.keras.metrics import MeanAbsoluteError
 from tensorflow.keras.losses import MeanSquaredError as mse
 
 class Baseline():
+
     def __init__(self):
         self.model = None
 
     def new_model(self, project_name = "baseline", tam = 512):
         self.model = Sequential(name = project_name)
         self.model.add(Input(shape = (tam, 1)))
-        self.model.add(Conv1D(filters = 32, kernel_size = 19, padding = "valid", activation = None, name = "kernel19", use_bias = False))
-        self.model.add(Conv1D(filters = 32, kernel_size = 17, padding = "valid", activation = None, name = "kernel17", use_bias = False))
+        self.model.add(Conv1D(filters = 32, kernel_size = 21, padding = "same", activation = None, name = "kernel21", use_bias = False))
+        self.model.add(Conv1D(filters = 32, kernel_size = 19, padding = "same", activation = None, name = "kernel19", use_bias = False))
         self.model.add(tf.keras.layers.MaxPooling1D(pool_size = 16, name = "MaxPool1D_1", data_format = "channels_first"))
-        # model.add(Conv1D(filters = 32, kernel_size = 19, padding = "valid", activation = None, name = "kernel19_2", use_bias = False))
-        # model.add(Conv1D(filters = 32, kernel_size = 17, padding = "valid", activation = None, name = "kernel17_2", use_bias = False))
-        # model.add(tf.keras.layers.MaxPooling1D(pool_size = 32, name = "MaxPool1D_2", data_format = "channels_first"))
-        self.model.add(tf.keras.layers.Flatten(name = "flatten"))
-        self.model.add(Dense(tam, activation = 'relu', name = "relu"))
-        # model.add(Dense(tam, activation = 'linear', name = "linear"))
+        self.model.add(tf.keras.layers.Flatten(name = "Flatten_1"))
+        self.model.add(tf.keras.layers.Reshape((2*tam, 1)))
+        self.model.add(Conv1D(filters = 32, kernel_size = 19, padding = "same", activation = None, name = "kernel19_2", use_bias = False))
+        self.model.add(tf.keras.layers.MaxPooling1D(pool_size = 32, name = "MaxPool1D_2", data_format = "channels_first"))
+        self.model.add(tf.keras.layers.Flatten(name = "Flatten_2"))
+        self.model.add(Dense(tam, activation = 'relu', name = "relu_1"))
 
-        # opt = optimizers.Adam(learning_rate = 0.0005, beta_1=0.9, beta_2 = 0.999, clipnorm = 0.45, epsilon=1.e-7)
-        opt = optimizers.Adam(learning_rate = 0.0005, beta_1=0.9, beta_2 = 0.999, clipnorm = 0.45, epsilon=1.e-7)
-        # opt = optimizers.SGD(learning_rate = 0.001, momentum=0.0, nesterov=False, name='SGD', clipnorm = 0.001)
-        # opt = optimizers.Adamax(learning_rate = 0.001, beta_1=0.9, beta_2=0.999, name='Adamax', clipnorm = 0.001)
+        opt = optimizers.Adamax(learning_rate = 0.0005, beta_1=0.9, beta_2=0.999, name='Adamax')
 
         loss = mse()
-        # loss = mae()
-        # metric1 = tf.keras.metrics.CategoricalAccuracy()
-        metric2 = MeanAbsoluteError()
-        self.model.compile(loss = loss, optimizer = opt, metrics = [metric2])
+        metric = MeanAbsoluteError()
+        self.model.compile(loss = loss, optimizer = opt, metrics = [metric])
         return self.model
--- tpc_signal/segmentation.py
+++ tpc_signal/segmentation.py
@@ -1,3 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import optimizers
+from tensorflow.keras.layers import Input, Dense, Conv1D
+from tensorflow.keras.metrics import MeanAbsoluteError
+from tensorflow.keras.losses import MeanSquaredError as mse
+
 class Segmentation():
+
     def __init__(self):
         self.model = None
+
+    def new_model(self, project_name = "segmentation", tam = 512):
+        input   = Input(shape = (tam, 1))
+        x       = Conv1D(filters=32, kernel_size=13, padding="same", activation = "sigmoid", name = "kernel13")(input)
+        x       = tf.keras.layers.MaxPooling1D(32, data_format = "channels_first")(x)
+        x       = tf.keras.layers.Flatten()(x)
+        outputs = Dense(tam, activation = 'sigmoid', name = "sigmoid")(x)
+        self.model   = tf.keras.Model(inputs=input, outputs=outputs, name=project_name)
+
+        opt = optimizers.Adam(learning_rate = 0.001, beta_1=0.9, beta_2 = 0.999, epsilon=1.e-7)
+
+        loss = tf.keras.losses.BinaryCrossentropy()
+        metric = tf.keras.metrics.BinaryAccuracy()
